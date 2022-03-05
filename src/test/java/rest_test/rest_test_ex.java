60c9e6f836f83ce11786bc23ee54718fdc5ba27c
moh-helal <moh.2014.helal@gmail.com>
2022-03-05 23:52:58 +0200

    first commit

--- src/test/java/rest_test/rest_test_ex.java
+++ src/test/java/rest_test/rest_test_ex.java
@@ -3,12 +3,11 @@ package rest_test;
 import java.util.HashMap;
 import java.util.Map;
 import org.json.simple.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
-
-
 
 public class rest_test_ex 
 {
@@ -26,17 +25,20 @@ public class rest_test_ex
 
 		System.out.println(r1);
         JsonPath useremail = new JsonPath(r1);
-        String emailAddress = useremail.getString("email");
-        System.out.println(emailAddress);
+        String email = useremail.getString("email");
+        System.out.println("the email address of the User  : " + email);
     }
 
     @Test(enabled=true)
     public void getposts() {
-        String r2 = given().contentType(ContentType.JSON).when().get("https://jsonplaceholder.typicode.com/posts?userId=3")
-                .then().assertThat().statusCode(200).extract().asString();
+        String r2 = given().
+        		contentType(ContentType.JSON).
+        		when().
+        		get("https://jsonplaceholder.typicode.com/posts?userId=3").
+        		then().assertThat().statusCode(200).extract().asString();
         
-        String postId = r2.toString();
-        System.out.println(postId);
+        String postID = r2.toString();
+        System.out.println("user’s associated posts : \n"+postID);
     }
     
     @Test(enabled=true)
@@ -45,14 +47,14 @@ public class rest_test_ex
     	Map <String,Object>map=new  HashMap<String,Object>();
     	JSONObject request=new JSONObject();
     	
-    	request.put("id"," 31");
-    	request.put("title"," helal");
-    	request.put("body"," i want to go");
+    	request.put("id"," 33");
+    	request.put("title"," Joe");
+    	request.put("body"," ncdncnc mdsnc");
     	request.put("userID"," 3");
     	System.out.println(request.toJSONString());
     	
     	baseURI="https://jsonplaceholder.typicode.com/";
-    	
-    	given().body(request.toJSONString()).when().post("/posts?userId=3").then().assertThat().statusCode(201).log().all(); 	    	
+    	given().body(request.toJSONString()).when().post("/posts?userId=3").then().assertThat().statusCode(201).log().all(); 
+		
     }
 }
